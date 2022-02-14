1543344cd3dcf49af70d27e0500204db8aa181d5
Pradip <pradipgiri329@gmail.com>
2022-02-14 10:31:52 +0545

    useState hook introduce

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState(0);
+  console.log(state);
+
+  const increment = () => {
+    setState(state + 1)
+  }
+  const reset = () => {
+    setState(0)
+  }
   return (
-    <div>Hello World</div>
-  );
+    <div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={reset}>Reset</button>
+      <h3>Incremented value: {state}</h3>
+    </div>
+  )
 }
+  
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
